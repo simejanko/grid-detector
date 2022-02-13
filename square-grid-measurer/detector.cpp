7bb960824088a9e17525d3e4f53dd94dcfcd5bd1
Simon Janezic <simon.janezic@gmail.com>
2022-02-13 09:01:50 +0100

    Add hough line transform and plan out algorithm for filtering grid lines

--- square-grid-measurer/detector.cpp
+++ square-grid-measurer/detector.cpp
@@ -2,13 +2,15 @@
 
 #include <opencv2/imgproc.hpp>
 
-SquareGridDetector::SquareGridDetector(cv::Scalar low_HSV_thresh, cv::Scalar high_HSV_thresh,
-                                       double canny_low_thresh_mul, double canny_high_thresh_mul,
-                                       int gauss_window_size, double gauss_sigma, int morph_close_size) :
+SquareGridDetector::SquareGridDetector(cv::Scalar low_HSV_thresh, cv::Scalar high_HSV_thresh, int min_visible_lines,
+                                       double grid_angle_tol, int min_line_votes, double canny_low_thresh_mul,
+                                       double canny_high_thresh_mul, int gauss_window_size, double gauss_sigma,
+                                       int morph_close_size) :
         low_HSV_thresh_(std::move(low_HSV_thresh)), high_HSV_thresh_(std::move(high_HSV_thresh)),
         gauss_window_size_(gauss_window_size, gauss_window_size), gauss_sigma_(gauss_sigma),
         morph_close_element_(cv::getStructuringElement(cv::MORPH_RECT, cv::Size(morph_close_size, morph_close_size))),
-        canny_low_thresh_mul_(canny_low_thresh_mul), canny_high_thresh_mul_(canny_high_thresh_mul) {}
+        canny_low_thresh_mul_(canny_low_thresh_mul), canny_high_thresh_mul_(canny_high_thresh_mul), min_line_votes_(min_line_votes),
+        min_visible_lines_(min_visible_lines), grid_angle_tol_(grid_angle_tol){}
 
 std::vector<cv::Point2f> SquareGridDetector::detect(const cv::Mat& image) {
     if (image.depth() != CV_8U || image.channels() != 3) { //accept RGB images
@@ -28,12 +30,13 @@ std::vector<cv::Point2f> SquareGridDetector::detect(const cv::Mat& image) {
               avg_brightness * canny_low_thresh_mul_, avg_brightness * canny_high_thresh_mul_);
     cv::bitwise_and(edge_mask_, mask_, edge_mask_); // apply the color mask
 
-    // hough transform
+    // hough lines transform (single-pixel & 1° resolution)
+    std::vector<cv::Vec3f> lines;
+    constexpr auto one_degree_rad = CV_PI/180;
+    cv::HoughLines(edge_mask_, lines, 1, one_degree_rad, min_line_votes_);
 
-    // nms magic (e.g. find the best big enough cluster of lines)
-
-    // intersections
-    return std::vector<cv::Point2f>();
+    // filter lines & find intersections
+    return grid_points(lines);
 }
 
 void SquareGridDetector::compute_color_mask(const cv::Mat& image) {
@@ -43,4 +46,20 @@ void SquareGridDetector::compute_color_mask(const cv::Mat& image) {
     cv::inRange(hsv_image_, low_HSV_thresh_, high_HSV_thresh_, mask_);
     // close holes in the mask
     cv::morphologyEx(mask_, mask_, cv::MORPH_CLOSE, morph_close_element_);
+
+    //TODO: could take the largest contour
+}
+
+std::vector<cv::Point2f> SquareGridDetector::grid_points(std::vector<cv::Vec3f>& lines) {
+    // bucket lines based on angles and accumulate votes (overlapping buckets to avoid edge fail cases)
+    //TODO: grid_angle_tol_
+
+    // take the 90 degree offset pair of buckets with highest geometric mean of votes (both buckets need to be decently large)
+
+    // isolate the first large enough group of lines (based on votes) in the two found buckets
+    //TODO: min_visible_lines_ + ratio test
+
+    // compute intersections
+
+    return {};
 }
--- square-grid-measurer/detector.hpp
+++ square-grid-measurer/detector.hpp
@@ -7,7 +7,8 @@
 
 class SquareGridDetector {
 public:
-    explicit SquareGridDetector(cv::Scalar low_HSV_thresh, cv::Scalar high_HSV_thresh, double canny_low_thresh_mul=0.66,
+    explicit SquareGridDetector(cv::Scalar low_HSV_thresh, cv::Scalar high_HSV_thresh, int min_visible_lines=5,
+                                double grid_angle_tol = CV_PI/30, int min_line_votes=100, double canny_low_thresh_mul=0.66,
                                 double canny_high_tresh_mul=1.33, int gauss_window_size=5, double gauss_sigma=1,
                                 int morph_close_size=9);
 
@@ -15,6 +16,8 @@ public:
 
 private:
     void compute_color_mask(const cv::Mat& image);
+
+    std::vector<cv::Point2f> grid_points(std::vector<cv::Vec3f>& lines);
 
     // parameters
     cv::Scalar low_HSV_thresh_;
@@ -24,6 +27,9 @@ private:
     cv::Mat morph_close_element_;
     double canny_low_thresh_mul_;
     double canny_high_thresh_mul_;
+    int min_line_votes_;
+    int min_visible_lines_;
+    double grid_angle_tol_;
 
     // intermediate results cache (to prevent always creating/resizing matrices)
     cv::Mat blurred_image_;
