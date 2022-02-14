76594697f1511a6e5487c0bb2ac16f27c8d82a19
Simon Janezic <simon.janezic@gmail.com>
2022-02-14 20:06:46 +0100

    Add kmeans to split lines into two groups based on angles (assumed horizontal & vertical)

--- square-grid-measurer/detector.cpp
+++ square-grid-measurer/detector.cpp
@@ -1,16 +1,17 @@
 #include "detector.hpp"
 
 #include <opencv2/imgproc.hpp>
+#include <opencv2/core.hpp>
 
 SquareGridDetector::SquareGridDetector(cv::Scalar low_HSV_thresh, cv::Scalar high_HSV_thresh, int min_visible_lines,
-                                       double grid_angle_tol, int min_line_votes, double canny_low_thresh_mul,
-                                       double canny_high_thresh_mul, int gauss_window_size, double gauss_sigma,
-                                       int morph_close_size) :
+                                       int min_line_votes, double canny_low_thresh_mul, double canny_high_thresh_mul,
+                                       int gauss_window_size, double gauss_sigma, int morph_close_size) :
         low_HSV_thresh_(std::move(low_HSV_thresh)), high_HSV_thresh_(std::move(high_HSV_thresh)),
         gauss_window_size_(gauss_window_size, gauss_window_size), gauss_sigma_(gauss_sigma),
         morph_close_element_(cv::getStructuringElement(cv::MORPH_RECT, cv::Size(morph_close_size, morph_close_size))),
-        canny_low_thresh_mul_(canny_low_thresh_mul), canny_high_thresh_mul_(canny_high_thresh_mul), min_line_votes_(min_line_votes),
-        min_visible_lines_(min_visible_lines), grid_angle_tol_(grid_angle_tol){}
+        canny_low_thresh_mul_(canny_low_thresh_mul), canny_high_thresh_mul_(canny_high_thresh_mul),
+        min_line_votes_(min_line_votes),
+        min_visible_lines_(min_visible_lines) {}
 
 std::vector<cv::Point2f> SquareGridDetector::detect(const cv::Mat& image) {
     if (image.depth() != CV_8U || image.channels() != 3) { //accept RGB images
@@ -25,14 +26,14 @@ std::vector<cv::Point2f> SquareGridDetector::detect(const cv::Mat& image) {
     // canny edges within a mask
     cv::cvtColor(blurred_image_, grayscale_image_, cv::COLOR_BGR2GRAY);
     // set canny thresholds relative to average brightness in masked area - very rough heuristic
-    double avg_brightness = cv::mean(grayscale_image_, mask_)(0);
+    auto avg_brightness = cv::mean(grayscale_image_, mask_)(0);
     cv::Canny(grayscale_image_, edge_mask_,
               avg_brightness * canny_low_thresh_mul_, avg_brightness * canny_high_thresh_mul_);
     cv::bitwise_and(edge_mask_, mask_, edge_mask_); // apply the color mask
 
     // hough lines transform (single-pixel & 1° resolution)
     std::vector<cv::Vec3f> lines;
-    constexpr auto one_degree_rad = CV_PI/180;
+    constexpr auto one_degree_rad = CV_PI / 180;
     cv::HoughLines(edge_mask_, lines, 1, one_degree_rad, min_line_votes_);
 
     // filter lines & find intersections
@@ -51,12 +52,22 @@ void SquareGridDetector::compute_color_mask(const cv::Mat& image) {
 }
 
 std::vector<cv::Point2f> SquareGridDetector::grid_points(std::vector<cv::Vec3f>& lines) {
-    // bucket lines based on angles and accumulate votes (overlapping buckets to avoid edge fail cases)
-    //TODO: grid_angle_tol_
+    // extract angles + a trick to mirror angles larger than 90 degrees for angle distances to make sense in euclidean space
+    // TODO: this may need to be stored in a (N, 1) cv::Mat for kmeans to work (cv::Mat(line_angles) should just work, without the need to copy)
+    std::vector<double> line_angles(lines.size());
+    std::transform(lines.begin(), lines.end(), line_angles.begin(),
+                   [](auto& line) { return line[1] <= CV_PI ? line[1] : 2 * CV_PI - line[1]; });
 
-    // take the 90 degree offset pair of buckets with highest geometric mean of votes (both buckets need to be decently large)
+    // cluster into 2 groups based on line angles
+    std::vector<int> clusters(line_angles.size());
+    auto kmeans_criteria = cv::TermCriteria(cv::TermCriteria::COUNT + cv::TermCriteria::EPS,
+                                            15, CV_PI / 360); // 0.5 degree criteria with max. 15 iterations
+    // 5 attempts with different cluster initializations
+    cv::kmeans(line_angles, 2, clusters, kmeans_criteria, 5,
+               cv::KmeansFlags::KMEANS_RANDOM_CENTERS);
 
-    // isolate the first large enough group of lines (based on votes) in the two found buckets
+
+    // isolate the first large enough group of lines (based on votes) in the two groups
     //TODO: min_visible_lines_ + ratio test
 
     // compute intersections
